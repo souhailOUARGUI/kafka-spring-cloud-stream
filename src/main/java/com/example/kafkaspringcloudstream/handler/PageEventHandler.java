5f0c4d00a93b98620f0ac01ffc670eddceb4f614
SouhailOUARGUI <ouargui550@gmail.com>
2025-10-31 13:11:37 +0100

    tested kStreamFunction

--- src/main/java/com/example/kafkaspringcloudstream/handler/PageEventHandler.java
+++ src/main/java/com/example/kafkaspringcloudstream/handler/PageEventHandler.java
@@ -1,12 +1,15 @@
 package com.example.kafkaspringcloudstream.handler;
 
 import com.example.kafkaspringcloudstream.events.PageEvent;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Component
@@ -26,6 +29,13 @@ public class PageEventHandler {
             return new PageEvent(Math.random()> 0.5? "P1":"P2",Math.random()> 0.5? "U1":"U2",new Date(),10 + new Random().nextInt(10000));
 
         };
+    }
+
+    @Bean
+    public Function<KStream<String, PageEvent>, KStream<String,Long>> kStreamFunction(){
+        return (input) ->
+            input.filter((k,v)-> v.duration()>100).map((k,v) -> new KeyValue<>(v.name(),v.duration()));
+
     }
 
 
