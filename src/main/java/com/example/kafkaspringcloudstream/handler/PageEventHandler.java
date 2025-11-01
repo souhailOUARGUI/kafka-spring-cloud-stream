46530e77c24d455c005e2902e8ed775a8581e736
SouhailOUARGUI <ouargui550@gmail.com>
2025-11-01 23:51:47 +0100

    added flux functo the controller

--- src/main/java/com/example/kafkaspringcloudstream/handler/PageEventHandler.java
+++ src/main/java/com/example/kafkaspringcloudstream/handler/PageEventHandler.java
@@ -1,11 +1,16 @@
 package com.example.kafkaspringcloudstream.handler;
 
 import com.example.kafkaspringcloudstream.events.PageEvent;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
@@ -34,7 +39,12 @@ public class PageEventHandler {
     @Bean
     public Function<KStream<String, PageEvent>, KStream<String,Long>> kStreamFunction(){
         return (input) ->
-            input.filter((k,v)-> v.duration()>100).map((k,v) -> new KeyValue<>(v.name(),v.duration()));
+            input.filter((k,v)-> v.duration()>100).
+                    map((k,v) -> new KeyValue<>(v.name(),v.duration())).
+                    groupByKey(Grouped.with(Serdes.String(),Serdes.Long())).
+                    windowedBy(TimeWindows.of(Duration.ofSeconds(5000))).
+                    count(Materialized.as("count-store")).
+                    toStream().map((k,v) -> new KeyValue<>(k.key(),v) );
 
     }
 
